2a768dd6cd96fa6c758d8475b7f3d7e5c1ad7126
dilonhenrique <dilonhenrique@gmail.com>
2023-05-03 15:49:52 -0300

    Env Variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  env: {
+    CU_KEY: process.env.CU_KEY,
+    CU_URL: process.env.CU_URL,
+    AS_KEY: process.env.AS_KEY,
+    AS_URL: process.env.AS_URL,
+    MYSQL_HOST: process.env.MYSQL_HOST,
+    MYSQL_DATABASE: process.env.MYSQL_DATABASE,
+    MYSQL_PORT: process.env.MYSQL_PORT,
+    MYSQL_USER: process.env.MYSQL_USER,
+    MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
+}
 }
 
 module.exports = nextConfig
